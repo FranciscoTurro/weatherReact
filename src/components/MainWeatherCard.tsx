866bdcefaa0fcde94c84883c0ab342d18af113e4
FranciscoTurro <franciscogtrro@outlook.com.ar>
2023-01-10 20:32:47 -0300

    working on ui

--- src/components/MainWeatherCard.tsx
+++ src/components/MainWeatherCard.tsx
@@ -1,4 +1,4 @@
-import uppercase from '../util/uppercase';
+import { uppercase } from '../util/utilFunctions';
 
 interface Props {
   mainTemp: number;
@@ -23,22 +23,24 @@ export const MainWeatherCard: React.FC<Props> = ({
     <div className="flex flex-col items-center mt-4 pt-20 pb-10">
       <h5 className="text-7xl font-semibold">{`${Math.round(mainTemp)} C°`}</h5>
       <img
-        className="w-36 h-36"
+        className="w-44 h-44"
         src={`http://openweathermap.org/img/wn/${iconID}@2x.png`}
         alt={description}
       />
-      <h5 className="mb-1 text-4xl font-medium pb-2">
-        {uppercase(description)}
-      </h5>
-      <span className="text-2xl">{`${cityName}, ${country}`}</span>
-      <div className="flex space-x-3 mt-4 gap-8">
-        <div className="flex flex-col items-center">
-          <h5 className="text-xl">{`${Math.round(minTemp)} C°`}</h5>
-          <h5>Min</h5>
-        </div>
-        <div className="flex flex-col items-center">
-          <h5 className="text-xl">{`${Math.round(maxTemp)} C°`}</h5>
-          <h5>Max</h5>
+      <div className="flex flex-col gap-3 items-center">
+        <h5 className="mb-1 text-4xl font-medium pb-2">
+          {uppercase(description)}
+        </h5>
+        <span className="text-2xl">{`${cityName}, ${country}`}</span>
+        <div className="flex space-x-3 mt-4 gap-12">
+          <div className="flex flex-col items-center">
+            <h5 className="text-xl">{`${Math.round(minTemp)} C°`}</h5>
+            <h5>Min</h5>
+          </div>
+          <div className="flex flex-col items-center">
+            <h5 className="text-xl">{`${Math.round(maxTemp)} C°`}</h5>
+            <h5>Max</h5>
+          </div>
         </div>
       </div>
     </div>
