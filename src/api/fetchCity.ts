7b1f4ca08b92ca8b847b626186ee1f5e0ea96218
FranciscoTurro <franciscogtrro@outlook.com.ar>
2023-01-17 18:40:31 -0300

    bug fixing

--- src/api/fetchCity.ts
+++ src/api/fetchCity.ts
@@ -6,7 +6,7 @@ export const fetchCity = async (
   const system = isMetric ? 'metric' : 'imperial';
 
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/${dataType}?q=${cityName}&units=${system}&appid=c59d34581182187599601a68c72dcacd`
+    `https://api.openweathermap.org/data/2.5/${dataType}?q=${cityName}&units=${system}&appid=c59d34581182187599601a68c72dcacd`
   );
   const data = await response.json();
   return data;
