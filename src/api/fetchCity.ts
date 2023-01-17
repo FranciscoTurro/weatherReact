9309dddf14ea709a83437d804fe2b341679316c6
FranciscoTurro <franciscogtrro@outlook.com.ar>
2023-01-17 17:45:47 -0300

    app has no backend, can't use env variables

--- src/api/fetchCity.ts
+++ src/api/fetchCity.ts
@@ -6,9 +6,7 @@ export const fetchCity = async (
   const system = isMetric ? 'metric' : 'imperial';
 
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/${dataType}?q=${cityName}&units=${system}&appid=${
-      import.meta.env.VITE_WEATHER_API_KEY
-    }`
+    `http://api.openweathermap.org/data/2.5/${dataType}?q=${cityName}&units=${system}&appid=c59d34581182187599601a68c72dcacd`
   );
   const data = await response.json();
   return data;
--- src/api/fetchCityName.ts
+++ src/api/fetchCityName.ts
@@ -3,9 +3,7 @@ export const fetchCityName = async (coordinates: {
   longitude: number;
 }) => {
   const response = await fetch(
-    `https://api.opencagedata.com/geocode/v1/json?q=${coordinates.latitude}+${
-      coordinates.longitude
-    }&key=${import.meta.env.VITE_GEOCODING_API_KEY}`
+    `https://api.opencagedata.com/geocode/v1/json?q=${coordinates.latitude}+${coordinates.longitude}&key=28d78b45dd72440eb7e41c836c972512`
   );
   const data = await response.json();
   return data;
